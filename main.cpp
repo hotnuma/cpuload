6272bf273997c7844fa17a603e139c154cec4b23
hotnuma <hotnuma@gmail.com>
2021-11-03 13:00:13 +0100

    -s option

--- main.cpp
+++ main.cpp
@@ -1,17 +1,44 @@
-#include <unistd.h>
-#include <print.h>
-
 #include "cpuload.h"
 
-int main()
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <print.h>
+
+int main(int argc, char **argv)
 {
+    int tick = 1;
+
+    int n = 1;
+    while (n < argc)
+    {
+        if (strcmp(argv[n], "-s") == 0)
+        {
+            if (++n >= argc)
+                return 1;
+
+            tick = atoi(argv[n]);
+        }
+        else
+        {
+            print("wrong option");
+            return 1;
+        }
+
+        n++;
+    }
+
+    if (tick < 1 || tick > 120)
+        tick = 1;
+
     while (1)
     {
         double cpu = cpuload();
 
         print("load : %lf", cpu);
 
-        sleep(1);
+        sleep(tick);
     }
 
     return 0;
